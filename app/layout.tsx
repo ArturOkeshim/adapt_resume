684c094a4bc16cb558d1bd8bb3f232cac9d092a3
ArturOkeshim <artem.mishchenko16@gmail.com>
2025-12-04 14:19:08 +0300

    Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 
 export const metadata = {
   title: 'Next.js',
@@ -12,6 +13,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S37G9YS43X"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S37G9YS43X');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
         <Analytics mode="production"/>
